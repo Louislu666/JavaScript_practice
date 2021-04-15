7fc7f4f1a6c963d5cfd513cbe92992c08ac93af0
Louislu666 <nt9510louis@163.com>
2021-04-15 13:11:03 +0800

    Cocos Creator学习

---
+++ TS/TSStduy/myFirstProject/assets/1.js
@@ -0,0 +1,21 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+
+    },
+
+    start () {
+        cc.systemEvent.on
+        (cc.SystemEvent.EventType.KEY_DOWN, 
+        this.onKeyDown, this);
+    },
+    onKeyDown: function (event) {
+        switch(event.keyCode) {
+            case cc.macro.KEY.a:
+                console.log('Press a key');
+                break;
+        }
+    },
+    // update (dt) {},
+});
